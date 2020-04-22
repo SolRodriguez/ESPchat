77f7b71c736778acea0f78240aa0176fb98b39c2
KImboslice <kimhdz25@gmail.com>
2020-04-22 18:09:23 -0400

    Updated espchat.py for a POST. File also on server

--- server/espchat.py
+++ server/espchat.py
@@ -1,2 +1,25 @@
 def request_handler(request):
-    pass
+    if request['method'] == 'GET':
+        pass
+
+    if request['method'] == 'POST':
+        #obtain byte data
+        data = request['data']
+
+        #decode and convert to string to break up
+        d = data.decode('utf-8')
+        split1 = d.split("=")
+        #['user', 'khernan5&img', '(D$\\x18D\\x18d\\xc5\\x01\\x80\\)']
+
+        split2 = split1[1].split("&")
+        #['khernan5', 'img']
+
+        username = split2[0]
+        img_data = split1[2]  
+        
+        ##unsure as to how the actual data is affected due to conversion. That is my conversion was done correctly
+    
+        
+        return (username ,img_data)
+
+        
